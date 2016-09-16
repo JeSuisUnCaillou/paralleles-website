d8ddff71308037af5ae9f9014b74a351da5d499e
Caillou <igorlacroute@gmail.com>
2016-09-16 12:52:25 +0000

    animate the 1->2 transition

--- app/assets/javascripts/paralleles_reader.js
+++ app/assets/javascripts/paralleles_reader.js
@@ -45,16 +45,19 @@ $(document).ready(function() {
         var canevas = SVG('canevas').size(width, height);
         var slider = canevas.group();
 
-        var create_frame = function(images_paths, next_images_paths, next_frames_paths, left_slide){
+        //left_slide is the number of slide right for the next button
+        //up_unsplit is the number of up_unsplit for the prev button
+        var create_frame = function(images_paths, next_images_paths, next_frames_paths, left_slide, up_unsplit){
             var frame_group = slider.group();
             var y = nb_slider_up * (frame_height + margin)
             var x = -1 * nb_slider_right * (frame_width + margin) / 2
             
             if(images_paths.length == 1){
                 x = x + width/4;
+                var down_split = next_images_paths.length < 2 ? 0 : 1
                 var image_group = draw_image(frame_group, images_paths[0], next_images_paths, x, y)
-                var next_button = draw_next_button(frame_group, next_frames_paths, frame_width, x, y, 0)
-                var prev_button = draw_prev_button(frame_group, frame_width, x, y, left_slide)
+                var next_button = draw_next_button(frame_group, next_frames_paths, frame_width, x, y, 0, down_split)
+                var prev_button = draw_prev_button(frame_group, frame_width, x, y, left_slide, up_unsplit)
             } else if(images_paths.length == 2) {
                 var left_images_paths = images_paths[0]
                 var right_images_paths = images_paths[1]
@@ -91,7 +94,7 @@ $(document).ready(function() {
                 //Draws the next button(s)
                 if(left_next_frames_paths.length > 0 && right_next_frames_paths.length > 0){
                     //One next button
-                    var next_button = draw_next_button(frame_group, next_frames_paths, width, x_next_button, y, 0)
+                    var next_button = draw_next_button(frame_group, next_frames_paths, width, x_next_button, y, 0, 0)
                 } else {
                     //Two next buttons, with one dumb
                     var x_dumb_button = x
@@ -107,12 +110,11 @@ $(document).ready(function() {
                         x_next_button = x
                         right_slide = 1
                     }
-                    var next_button = draw_next_button(frame_group, frames_paths, frame_width, x_next_button, y, right_slide)
+                    var next_button = draw_next_button(frame_group, frames_paths, frame_width, x_next_button, y, right_slide, 0)
                     var dumb_button = draw_dumb_button(frame_group, frame_width, x_dumb_button, y)
                 }
                 //Draws the prev button
-                console.log(left_slide)
-                var prev_button = draw_prev_button(frame_group, prev_button_width, x_prev_button, y, left_slide)
+                var prev_button = draw_prev_button(frame_group, prev_button_width, x_prev_button, y, left_slide, up_unsplit)
                 
             } else {
                 console.log("Only one or two images_paths accepted. Actual length : " + images_paths.length)
@@ -134,7 +136,7 @@ $(document).ready(function() {
         };
         
         //Draw a next button in a parent_group, at a given position
-        var draw_prev_button = function(parent_group, button_width, x, y, left_slide){
+        var draw_prev_button = function(parent_group, button_width, x, y, left_slide, up_unsplit){
             //Do not create a prev button if we havent moved up the slider (aka : for the first frame)
             if(nb_slider_up == 0){ return null; }
             var prev_button = parent_group.rect(button_width, button_height).attr({ fill: 'grey' }).addClass('hoverable').translate(x, y)
@@ -143,7 +145,7 @@ $(document).ready(function() {
             prev_button.click(function(){
                 nb_slider_down = nb_slider_down + 1
                 nb_slider_right = nb_slider_right - left_slide
-                var with_movement = (left_slide != 0)
+                var with_movement = left_slide != 0 || up_unsplit != 0
                 make_it_slide(with_movement)
             })
         }
@@ -155,14 +157,14 @@ $(document).ready(function() {
         }
         
         //Draw a next button in a parent_group, at a given position
-        var draw_next_button = function(parent_group, next_frames_paths, button_width, x, y, right_slide){
+        var draw_next_button = function(parent_group, next_frames_paths, button_width, x, y, right_slide, down_split){
             //Do not create a next button if next_frames_paths is empty
-            //console.log(JSON.stringify(right_slide))
+            //console.log(JSON.stringify(down_split))
             if(next_frames_paths == [undefined, undefined] || next_frames_paths.length == 0){ return null; }
             
             var next_button = parent_group.rect(button_width, button_height).attr({ fill: 'grey' }).addClass('hoverable').translate(x, y + frame_height - button_height)
             var next_arrow = parent_group.polyline('0,0 50,50 100,0').translate(x + button_width / 2 - 50, y + frame_height - button_height/2 - 25).fill('none').stroke({ width: 5, color: "blue" })
-            var with_movement = (right_slide != 0)
+            var with_movement = (right_slide != 0) || down_split != 0
             
             next_button.click(function(){
                 //If we have already moved down the slider, just move it up without creating a next frame
@@ -178,7 +180,7 @@ $(document).ready(function() {
                     nb_slider_right = nb_slider_right + right_slide
                     $.ajax({url: next_frames_paths[0], success: function(result){
                         //create the frame
-                        create_frame(result["images_paths"], result["next_images_paths"], result["next_frames_paths"], right_slide)
+                        create_frame(result["images_paths"], result["next_images_paths"], result["next_frames_paths"], right_slide, down_split)
                         //move up the slider
                         make_it_slide(with_movement)
                     }, error: function(){
@@ -201,8 +203,9 @@ $(document).ready(function() {
                             var right_images_paths = result["images_paths"]
                             var right_next_frames_paths = result["next_frames_paths"]
                             var right_next_images_paths = result["next_images_paths"]
+
                             //create the frame
-                            create_frame([left_images_paths, right_images_paths], left_next_images_paths.concat(right_next_images_paths), [left_next_frames_paths, right_next_frames_paths], right_slide)
+                            create_frame([left_images_paths, right_images_paths], left_next_images_paths.concat(right_next_images_paths), [left_next_frames_paths, right_next_frames_paths], right_slide, down_split)
                             //move up the slider
                             make_it_slide(with_movement)
                         }, error: function(){
